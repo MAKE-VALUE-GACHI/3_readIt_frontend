c6625746341e2f2ea1d6b90b8aff7b68fb06e454
ggujin <yang97816@gmail.com>
2025-06-27 21:26:01 +0900

    feat: add global font. Pretendard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
 import Provider from './provider'
+import localFont from 'next/font/local'
 import './globals.css'
+
+const pretendard = localFont({
+  src: '../public/fonts/pretendard/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '100 900',
+  variable: '--font-pretendard',
+})
 
 export default function RootLayout({
   children,
@@ -7,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="ko">
-      <body>
+    <html lang="ko" className={`${pretendard.variable}`}>
+      <body className={pretendard.className}>
         <Provider>{children}</Provider>
       </body>
     </html>
