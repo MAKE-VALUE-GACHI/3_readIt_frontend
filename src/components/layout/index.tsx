d391623fbeabd7a2c8e937570910310743da853c
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2025-07-05 23:07:50 +0900

    feat: sidebar & layout 개발 완료

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import { Sidebar } from "@/components/common/sidebar";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="relative flex h-screen w-screen gap-4 p-4">
+      {/**gradation layer */}
+      <div className="absolute inset-0 -z-10 bg-gradient-to-br from-[#d1b3ff] via-[#b266ff] to-[#b266ff]" />
+      <div className="absolute inset-0 -z-10 bg-gradient-to-tl from-[#ff6f91] to-transparent opacity-70 mix-blend-overlay" />
+      <Sidebar />
+      {children}
+    </main>
+  );
+};
+
+export default Layout;
