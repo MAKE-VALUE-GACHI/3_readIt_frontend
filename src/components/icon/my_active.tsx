dc51d7bfe747b587e87bee5148736f7b14d7f0dc
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2025-07-05 23:06:49 +0900

    feat: icon 컴포넌트 추가

---
+++ src/components/icon/my_active.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const My_active = () => {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M8.8 4H14.2C18.7 4 20.5 5.83486 20.5 10.422V15.9266C20.5 19.3945 19.474 21.2936 17.026 22H5.974C3.526 21.2936 2.5 19.3945 2.5 15.9266V10.422C2.5 5.83486 4.3 4 8.8 4Z"
+        fill="#774EF0"
+        stroke="#774EF0"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M18 21C17.7671 18.2043 14.9406 16 11.5 16C8.05945 16 5.2329 18.2043 5 21"
+        stroke="#EEEEEE"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.6322 10.2585C14.6322 11.991 13.2322 13.3997 11.4997 13.3997C9.76719 13.3997 8.36719 11.991 8.36719 10.2585C8.36719 8.52599 9.76719 7.12598 11.4997 7.12598C13.2322 7.12598 14.6322 8.52599 14.6322 10.2585Z"
+        stroke="#EEEEEE"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default My_active
