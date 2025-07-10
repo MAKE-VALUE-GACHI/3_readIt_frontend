5dd5f7493498bbf44f12c6f005cd5ff427a0499b
ggujin <yang97816@gmail.com>
2025-07-10 16:20:41 +0900

    style: add summage page style

--- src/app/(main)/summary/page.tsx
+++ src/app/(main)/summary/page.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import Image from 'next/image'
 
 const SummaryPage = () => {
-  return <div></div>
+  return (
+    <div className="flex min-h-full w-full">
+      <div className="relative mr-4 basis-1/3 rounded-xl bg-white p-10">
+        <div>
+          <h2 className="mb-5 text-xl font-bold text-[#222222]">요약</h2>
+          <div className="mb-3">
+            <label className="text-base font-semibold">링크 요약 정리</label>
+            <div className="bg-[#F8F8F8] p-3">
+              <input type="text" placeholder="https://" />
+            </div>
+          </div>
+
+          <div>
+            <label className="text-base font-semibold">내용 정리</label>
+            <div className="rounded-xl bg-[#F8F8F8] p-3">
+              <textarea className="min-h-70" placeholder="요약하고 싶은 내용을 입력해 주세요."></textarea>
+              <div className="text-end text-sm text-[#AAAAAA]">0/500</div>
+            </div>
+          </div>
+        </div>
+
+        <button className="absolute inset-x-10 bottom-10 h-14 rounded-lg bg-[#F8F8F8] text-lg font-semibold text-[#aaaaaa]">
+          자동완성
+        </button>
+      </div>
+
+      <div className="relative basis-2/3 rounded-xl bg-white p-10">
+        <div className="absolute bottom-40 left-1/2 -translate-x-1/2">
+          <Image src="/assets/summary.svg" alt="text" width={329} height={84} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default SummaryPage
