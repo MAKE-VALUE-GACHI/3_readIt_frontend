f3d0c77f38e0ad3e1a446d81b8048c8d2245a404
ggujin <yang97816@gmail.com>
2025-07-10 16:27:34 +0900

    style: add 사이드바 최소너비

--- src/components/common/sidebar/Sidebar.tsx
+++ src/components/common/sidebar/Sidebar.tsx
@@ -14,7 +14,7 @@ const Sidebar = () => {
   const [openMyMenu, setOpenMyMenu] = useState(false)
 
   return (
-    <main className="flex h-full w-[272px] flex-col justify-between rounded-xl bg-white p-5">
+    <main className="flex h-full min-w-[272px] flex-col justify-between rounded-xl bg-white p-5">
       <section className="flex flex-col items-center gap-5 pt-2">
         <Image width={114} height={50} src={'/assets/logo.png'} alt="logo" />
         <section className="flex w-full flex-col gap-2">
