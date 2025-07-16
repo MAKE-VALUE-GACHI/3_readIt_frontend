2cfc6ce72c0d297a6d40f8212ab898666c63675f
ggujin <yang97816@gmail.com>
2025-07-16 22:38:03 +0900

    style: main page

---
+++ src/components/icon/DocumentCode.tsx
@@ -0,0 +1,27 @@
+const DocumentCode = () => {
+  return (
+    <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M21 7.5V17.5C21 20.5 19.5 22.5 16 22.5H8C4.5 22.5 3 20.5 3 17.5V7.5C3 4.5 4.5 2.5 8 2.5H16C19.5 2.5 21 4.5 21 7.5Z"
+        stroke="#171717"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M7 9.5H12" stroke="#171717" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M7 13.5H16" stroke="#171717" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M7 17.5H16" stroke="#171717" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path
+        d="M14.5 5V7C14.5 8.1 15.4 9 16.5 9H18.5"
+        stroke="#171717"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default DocumentCode
