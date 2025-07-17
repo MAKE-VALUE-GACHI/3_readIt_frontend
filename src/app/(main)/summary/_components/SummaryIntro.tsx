0ab39dd3e27c797cbd3a07c4a63dafefe8dc774f
ggujin <yang97816@gmail.com>
2025-07-17 14:12:22 +0900

    refactor: extra summary intro component

---
+++ src/app/(main)/summary/_components/SummaryIntro.tsx
@@ -0,0 +1,9 @@
+import { SummaryTextIntro } from '@/components/icon'
+
+export function SummaryIntro() {
+  return (
+    <div className="absolute bottom-40 left-1/2 -translate-x-1/2">
+      <SummaryTextIntro />
+    </div>
+  )
+}
