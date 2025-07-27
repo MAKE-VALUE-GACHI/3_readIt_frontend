5a70502dd82660ede25d079de31b6791a0500a67
ggujin <yang97816@gmail.com>
2025-07-27 17:00:18 +0900

    feat: update button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -8,6 +8,7 @@ import { RotateIcon } from '@/components/motion'
 
 export interface ReaditButtonProps extends React.ComponentProps<'button'>, VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  centerText?: boolean
 
   //Icon 추가 시 사용
   defaultIcon?: React.ReactNode
@@ -27,6 +28,8 @@ const buttonVariants = cva(
         SUMMARY_BUTTON: 'px-4 py-2 rounded-full bg-white text-black active:text-white active:bg-black',
         MAIN_SIDE_BUTTON:
           'flex items-center gap-2 w-full px-2 py-2 text-sm font-medium rounded-md bg-white text-muted-foreground hover:bg-gray-light hover:text-primary active:bg-gray-light active:text-primary',
+        SUMMARY_SUBMIT_BUTTON:
+          'absolute inset-x-10 bottom-10 h-14 rounded-lg text-lg font-semibold bg-gray-extraLight text-gray-medium',
       },
     },
     defaultVariants: {
@@ -44,6 +47,7 @@ function Button({
   directionIcon = false,
   isActive = false,
   asChild = false,
+  centerText = false,
   ...props
 }: ReaditButtonProps) {
   const Comp = asChild ? Slot : 'button'
@@ -51,10 +55,10 @@ function Button({
   return (
     <Comp
       data-slot="button"
-      className={cn(buttonVariants({ variant, className }), isActive && 'bg-gray-light text-primary')}
+      className={cn(isActive && 'bg-gray-light text-primary', buttonVariants({ variant, className }))}
       {...props}
     >
-      <div className="flex w-full items-center justify-between">
+      <div className={`flex w-full items-center justify-between ${centerText && 'justify-center'}`}>
         <span className="flex items-center gap-4">
           {
             // isActive 상태에 따라 아이콘 표시해줌
@@ -64,12 +68,7 @@ function Button({
         </span>
         {directionIcon && (
           <RotateIcon isRotated={isActive} rotation={180}>
-            <ChevronDownIcon
-              className={cn(
-                'text-muted-foreground size-4',
-                isActive && 'text-primary',
-              )}
-            />
+            <ChevronDownIcon className={cn('text-muted-foreground size-4', isActive && 'text-primary')} />
           </RotateIcon>
         )}
       </div>
